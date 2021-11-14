680aaf0efe69928225780cb1dae7042ddb3cb345
Ryan Bester <ryan.bester5@gmail.com>
2021-11-14 18:14:53 +0000

    Initial commit

---
+++ Waveboard.Resources/ResourceAssembly.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Waveboard.Resources
+{
+    public static class WaveboardResources
+    {
+        public static Assembly ResourceAssembly => typeof(WaveboardResources).Assembly;
+    }
+}
---
+++ Waveboard.Windows/UI/Extensions/ImageResourceExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+
+namespace Waveboard.UI.Extensions
+{
+    [ContentProperty (nameof(Source))]
+    public class ImageResourceExtension : MarkupExtension
+    {
+        public ImageResourceExtension(string source)
+        {
+            Source = source;
+        }
+
+        public string Source { get; set;}
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var bmp = new Bitmap(Waveboard.Resources.WaveboardResources.ResourceAssembly.GetManifestResourceStream(Source));
+
+            return Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+    }
+}
